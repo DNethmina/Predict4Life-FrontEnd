dfb8fdb8e356d877e2ef9d98a13b69f87a44dbb3
DNethmina <deshannethmina2001@gmail.com>
2025-10-18 19:19:38 +0530

    signup page UI create

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Import both pages
 import Login from './pages/login/login.jsx'
+import Signup from './pages/signup/signup.jsx'; // 1. Import the Signup component
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Existing routes */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+
+        {/* 2. Add the new route for the signup page */}
+        <Route path="/signup" element={<Signup />} />
+        
       </Routes>
     </Router>
   )
 }
 
-export default App
+export default App;
